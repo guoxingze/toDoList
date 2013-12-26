3f038b3bc16547b140352f5dd038d99b293e87a2
guoxingze <guoxingze@gmail.com>
2013-12-26 11:47:45 -0600

    test expandable listview

--- gen/com/example/todolist/R.java
+++ gen/com/example/todolist/R.java
@@ -24,20 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000f;
         public static final int button1=0x7f080002;
-        public static final int item_checkbox=0x7f080007;
-        public static final int item_textview=0x7f080008;
-        public static final int listviewLayout=0x7f080006;
+        public static final int childListView_checkbox_main=0x7f080008;
+        public static final int childListView_main=0x7f080006;
+        public static final int childListView_textview_main=0x7f080007;
+        public static final int item_checkbox=0x7f08000a;
+        public static final int item_textview=0x7f08000b;
+        public static final int listviewLayout=0x7f080009;
         public static final int main_BottomLayout=0x7f080005;
         public static final int main_MidLayout=0x7f080003;
-        public static final int main_listview=0x7f080004;
+        public static final int main_exlistview=0x7f080004;
         public static final int main_topLayout=0x7f080000;
+        public static final int parentListView_checkbox_main=0x7f08000e;
+        public static final int parentListView_main=0x7f08000c;
+        public static final int parentListView_textview_main=0x7f08000d;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int listview01=0x7f030001;
+        public static final int childlistview_main=0x7f030001;
+        public static final int listview01=0x7f030002;
+        public static final int parentlistview_main=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/todolist/MainActivity.java
+++ src/com/example/todolist/MainActivity.java
@@ -6,8 +6,11 @@ import java.util.HashMap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.CheckBox;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.SimpleExpandableListAdapter;
 
 public class MainActivity extends Activity {
 
@@ -17,9 +20,12 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		//Assign listview
-		ListView list =(ListView) findViewById(R.id.main_listview);
-		SimpleAdapter listAdapter = this.listviewSetUp();
+		ExpandableListView list =(ExpandableListView) findViewById(R.id.main_exlistview);
+		SimpleExpandableListAdapter listAdapter = this.listviewSetUp();
 		list.setAdapter(listAdapter);
+		list.expandGroup(0);
+
+
 		
 		
 	}
@@ -34,23 +40,72 @@ public class MainActivity extends Activity {
 	/**
 	 * initialize listview in main activity
 	 */
-	public SimpleAdapter listviewSetUp(){
+	public SimpleExpandableListAdapter  listviewSetUp(){
 		ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String,Object>>();
 		for(int i = 0;i<10;i++){
 			HashMap<String,Object> map = new HashMap<String,Object>();
 			if(i%2==0)
-				map.put("Done", true);
+				map.put("PDone", true);
 			else
-				map.put("Done", false);
+				map.put("PDone", false);
 			
-			map.put("Content", "This is" + i);
+			map.put("PContent", "This is" + i);
 			
 			listItem.add(map);	
 		}
-		//ListView Adapter
-		SimpleAdapter listItemAdapter = new SimpleAdapter(this, listItem, R.layout.listview01, 
-					new String[]{"Done","Content"}, new int[]{R.id.item_checkbox,R.id.item_textview});
+		//Child
+		ArrayList<ArrayList<HashMap<String,Object>>> childs = new ArrayList<ArrayList<HashMap<String,Object>>>();
+		for(int i =0;i<10;i++){
+			ArrayList<HashMap<String,Object>> subCHild = new ArrayList<HashMap<String,Object>>();
+			for(int j =0; j<3;j++){
+				HashMap<String,Object> childMap = new HashMap<String,Object>();
+				if(j%2==0)
+					childMap.put("CDone", true);
+				else
+					childMap.put("CDone", false);
+				
+				childMap.put("CContent", "This is" + j);
+				
+				subCHild.add(childMap);	
+			}
+			childs.add(subCHild);
+		}
 		
+//		 /**
+//         * 使用SimpleExpandableListAdapter显示ExpandableListView
+//         * 参数1.上下文对象Context
+//         * 参数2.一级条目目录集合
+//         * 参数3.一级条目对应的布局文件
+//         * 参数4.fromto，就是map中的key，指定要显示的对象
+//         * 参数5.与参数4对应，指定要显示在groups中的id
+//         * 参数6.二级条目目录集合
+//         * 参数7.二级条目对应的布局文件
+//         * 参数8.fromto，就是map中的key，指定要显示的对象
+//         * 参数9.与参数8对应，指定要显示在childs中的id
+//         */
+//		ListView Adapter
+//		SimpleExpandableListAdapter  listItemAdapter = new SimpleExpandableListAdapter(this, listItem, R.layout.listview01, 
+//					new String[]{"Done","Content"}, new int[]{R.id.item_checkbox,R.id.item_textview});
+		
+//		SimpleExpandableListAdapter  listItemAdapter = new SimpleExpandableListAdapter(this,
+//				listItem,
+//				R.layout.parentlistview_main,
+//				new String[]{"PDone","PContent"}, 
+//				new int[]{R.id.parentListView_checkbox_main,R.id.parentListView_textview_main},
+//				childs,
+//				R.layout.childlistview_main,
+//				new String[]{"CDone","CContent"},
+//				new int[]{R.id.childListView_checkbox_main,R.id.childListView_textview_main});
+		
+		SimpleExpandableListAdapter  listItemAdapter = new SimpleExpandableListAdapter(this,
+				listItem,
+				R.layout.parentlistview_main,
+				new String[]{"PContent"}, 
+				new int[]{R.id.parentListView_textview_main},
+				childs,
+				R.layout.childlistview_main,
+				new String[]{"CContent","CDone"},
+				new int[]{R.id.childListView_textview_main,R.id.childListView_checkbox_main});
 		return listItemAdapter;
 	}
 	
